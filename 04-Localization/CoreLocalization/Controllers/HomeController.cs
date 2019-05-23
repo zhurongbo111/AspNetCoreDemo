efd92e1f52a80f3a392bd6a6b3ae015c83d2643d
Gary Zhu <ZHUGX@legal.regn.net>
2019-05-23 15:54:49 +0800

    Add Localization

---
+++ 04-Localization/CoreLocalization/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoreLocalization.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Localization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Builder;
+
+namespace CoreLocalization.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStringLocalizer<HomeController> _localizer;
+
+        public HomeController(IStringLocalizer<HomeController> localizer)
+        {
+            _localizer = localizer;
+            
+        }
+
+        public IActionResult Index()
+        {
+            var cultureFeature = HttpContext.Features.Get<IRequestCultureFeature>();
+            var word = _localizer.GetString("Hello");
+
+            var zhLocalizer = _localizer.WithCulture(new System.Globalization.CultureInfo("zh-CN"));
+            var enLocalizer = _localizer.WithCulture(new System.Globalization.CultureInfo("en-US"));
+            word = zhLocalizer.GetString("Hello");
+            word = enLocalizer.GetString("Hello");
+            word = _localizer.GetString("Hello");
+
+            System.Globalization.CultureInfo.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+            word = zhLocalizer.GetString("Hello");
+            word = enLocalizer.GetString("Hello");
+            word = _localizer.GetString("Hello");
+
+            System.Globalization.CultureInfo.CurrentUICulture = new System.Globalization.CultureInfo("zh-CN");
+            word = zhLocalizer.GetString("Hello");
+            word = enLocalizer.GetString("Hello");
+            word = _localizer.GetString("Hello");
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 04-Localization/CoreLocalization/Startup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreLocalization
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddLocalization(localizationOption => {
+                localizationOption.ResourcesPath = "MyResources";
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var supportedCultures = new[] { "zh-CN", "en-US" };
+            app.UseRequestLocalization((requestLocalizationOptions) =>
+            {
+                requestLocalizationOptions.RequestCultureProviders.Clear();
+                requestLocalizationOptions.RequestCultureProviders.Add(new CookieRequestCultureProvider() { Options = requestLocalizationOptions });
+                requestLocalizationOptions.AddSupportedCultures(supportedCultures);
+                requestLocalizationOptions.AddSupportedUICultures(supportedCultures);
+                requestLocalizationOptions.SetDefaultCulture(supportedCultures[0]);
+
+                //Optionally create an app-specific provider with just a delegate, e.g. look up user preference from DB.
+                //requestLocalizationOptions.RequestCultureProviders.Insert(0, 
+                //    new CustomRequestCultureProvider(
+                //        async context =>{
+                //            return await Task.FromResult(new ProviderCultureResult("zh-CN"));
+                //        }
+                //    )
+                //);
+
+            });
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
