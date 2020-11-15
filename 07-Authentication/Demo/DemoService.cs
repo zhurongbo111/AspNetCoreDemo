703e5a7e4c1b70adeb1d95076f636456b69cb3b0
Gary Zhu <390992476@qq.com>
2020-11-15 20:08:08 +0800

    Add Auth

---
+++ 07-Authentication/Demo/DemoService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class DemoService : AuthenticationService, IAuthenticationService
+    {
+        public DemoService(IAuthenticationSchemeProvider schemes, IAuthenticationHandlerProvider handlers, IClaimsTransformation transform, IOptions<AuthenticationOptions> options)
+           : base(schemes, handlers, transform, options)
+        {
+  
+        }
+
+        async Task<AuthenticateResult> IAuthenticationService.AuthenticateAsync(HttpContext context, string scheme)
+        {
+            var result = await base.AuthenticateAsync(context, scheme);
+            return result;
+        }
+
+        Task IAuthenticationService.ChallengeAsync(HttpContext context, string scheme, AuthenticationProperties properties)
+        {
+            return base.ChallengeAsync(context, scheme, properties);
+        }
+
+        Task IAuthenticationService.ForbidAsync(HttpContext context, string scheme, AuthenticationProperties properties)
+        {
+            return base.ForbidAsync(context, scheme, properties);
+        }
+
+        Task IAuthenticationService.SignInAsync(HttpContext context, string scheme, ClaimsPrincipal principal, AuthenticationProperties properties)
+        {
+            return base.SignInAsync(context, scheme,principal, properties);
+        }
+
+        Task IAuthenticationService.SignOutAsync(HttpContext context, string scheme, AuthenticationProperties properties)
+        {
+            return base.SignOutAsync(context, scheme, properties);
+        }
+    }
+}
---
+++ 07-Authentication/Demo/Startup.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Demo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, ops => {
+                ops.Authority = "https://cognito-idp.ap-southeast-1.amazonaws.com/ap-southeast-1_30JpWzjHm";
+                ops.Audience = "ap-southeast-1_30JpWzjHm";
+            });
+
+            services.Replace(ServiceDescriptor.Scoped<IAuthenticationService, DemoService>());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
